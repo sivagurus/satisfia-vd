de963a164b389a52c2188bba6b169c6f2a82a14b
sivagurus <sivaguru1647@gmail.com>
2020-12-26 19:09:41 +0530

    try catch

--- guru/plugins/incoming_message_fn.py
+++ guru/plugins/incoming_message_fn.py
@@ -32,119 +32,129 @@ from guru.helper_funcs.upload_to_tg import upload_to_tg
 
 async def incoming_youtube_dl_f(client, message):
     """ /ytdl command """
-    g_id = message.from_user.id
-    credit = await message.reply_text(f"💀 Downloading for you <a href='tg://user?id={g_id}'>🤕</a>", parse_mode="html")
-    i_m_sefg = await message.reply_text("processing", quote=True)
-    # LOGGER.info(message)
-    # extract link from message
-    dl_url, cf_name, yt_dl_user_name, yt_dl_pass_word = await extract_link(
-        message.reply_to_message, "YTDL"
-    )
-    LOGGER.info(dl_url)
-    #if len(message.command) > 1:
-        #if message.command[1] == "gdrive":
-            #with open('blame_my_knowledge.txt', 'w+') as gg:
-                #gg.write("I am noob and don't know what to do that's why I have did this")
-    LOGGER.info(cf_name)
-    if dl_url is not None:
-        await i_m_sefg.edit_text("extracting links")
-        current_user_id = message.from_user.id
-        # create an unique directory
-        user_working_dir = os.path.join(DOWNLOAD_LOCATION, str(current_user_id))
-        # create download directory, if not exist
-        if not os.path.isdir(user_working_dir):
-            os.makedirs(user_working_dir)
-        # list the formats, and display in button markup formats
-        thumb_image, text_message, reply_markup = await extract_youtube_dl_formats(
-            dl_url,
-            cf_name,
-            yt_dl_user_name,
-            yt_dl_pass_word,
-            user_working_dir
+    try:
+        g_id = message.from_user.id
+        credit = await message.reply_text(f"💀 Downloading for you <a href='tg://user?id={g_id}'>🤕</a>", parse_mode="html")
+        i_m_sefg = await message.reply_text("processing", quote=True)
+        # LOGGER.info(message)
+        # extract link from message
+        dl_url, cf_name, yt_dl_user_name, yt_dl_pass_word = await extract_link(
+            message.reply_to_message, "YTDL"
         )
-        print(thumb_image)
-        req = requests.get(f"{thumb_image}")
-        gau_tam = f"{current_user_id}.jpg"
-        open(gau_tam, 'wb').write(req.content)
-        if thumb_image is not None:
-            await message.reply_photo(
-                #text_message,
-                photo=gau_tam,
-                quote=True,
-                caption=text_message,
-                reply_markup=reply_markup
+        LOGGER.info(dl_url)
+        #if len(message.command) > 1:
+            #if message.command[1] == "gdrive":
+                #with open('blame_my_knowledge.txt', 'w+') as gg:
+                    #gg.write("I am noob and don't know what to do that's why I have did this")
+        LOGGER.info(cf_name)
+        if dl_url is not None:
+            await i_m_sefg.edit_text("extracting links")
+            current_user_id = message.from_user.id
+            # create an unique directory
+            user_working_dir = os.path.join(DOWNLOAD_LOCATION, str(current_user_id))
+            # create download directory, if not exist
+            if not os.path.isdir(user_working_dir):
+                os.makedirs(user_working_dir)
+            # list the formats, and display in button markup formats
+            thumb_image, text_message, reply_markup = await extract_youtube_dl_formats(
+                dl_url,
+                cf_name,
+                yt_dl_user_name,
+                yt_dl_pass_word,
+                user_working_dir
             )
-            await i_m_sefg.delete()
+            print(thumb_image)
+            req = requests.get(f"{thumb_image}")
+            gau_tam = f"{current_user_id}.jpg"
+            open(gau_tam, 'wb').write(req.content)
+            if thumb_image is not None:
+                await message.reply_photo(
+                    #text_message,
+                    photo=gau_tam,
+                    quote=True,
+                    caption=text_message,
+                    reply_markup=reply_markup
+                )
+                await i_m_sefg.delete()
+            else:
+                await i_m_sefg.edit_text(
+                    text=text_message,
+                    reply_markup=reply_markup
+                )
         else:
             await i_m_sefg.edit_text(
-                text=text_message,
-                reply_markup=reply_markup
+                "**FCUK**! wat have you entered. \nPlease read /help \n"
+                f"<b>API Error</b>: {cf_name}"
             )
-    else:
-        await i_m_sefg.edit_text(
-            "**FCUK**! wat have you entered. \nPlease read /help \n"
-            f"<b>API Error</b>: {cf_name}"
-        )
+    except Exception as g_e:
+        LOGGER.info(g_e)
+    
 #playlist
 async def g_yt_playlist(client, message):
     """ /pytdl command """
     #i_m_sefg = await message.reply_text("Processing...you should wait🤗", quote=True)
-    usr_id = message.from_user.id
-    G_DRIVE = False
-    if len(message.command) > 1:
-        if message.command[1] == "gdrive":
-            G_DRIVE = True
-    if 'youtube.com/playlist' in message.reply_to_message.text:
-        i_m_sefg = await message.reply_text("Downloading...you should wait🤗", quote=True)
-        await yt_playlist_downg(message.reply_to_message, i_m_sefg, G_DRIVE)
-    
-    else:
-        await message.reply_text("Reply to youtube playlist link only 🙄")
+    try:
+        usr_id = message.from_user.id
+        G_DRIVE = False
+        if len(message.command) > 1:
+            if message.command[1] == "gdrive":
+                G_DRIVE = True
+        if 'youtube.com/playlist' in message.reply_to_message.text:
+            i_m_sefg = await message.reply_text("Downloading...you should wait🤗", quote=True)
+            await yt_playlist_downg(message.reply_to_message, i_m_sefg, G_DRIVE)
+        
+        else:
+            await message.reply_text("Reply to youtube playlist link only 🙄")
+    except Exception as g_e:
+        LOGGER.info(g_e)
                 
 async def rename_tg_file(client, message):
-    usr_id = message.from_user.id
-    if len(message.command) > 1:
-        new_name = '/app/' + message.command[1].strip()
-        file = await download_tg(client, message)
-        try:
-            if file:
-                os.rename(file, new_name)
-        except Exception as g_g:
-            await message.reply_text("g_g")
-        response = {}
-        final_response = await upload_to_tg(
-            message,
-            new_name,
-            usr_id,
-            response
-        )
-        LOGGER.info(final_response)
-        try:
-            message_to_send = ""
-            for key_f_res_se in final_response:
-                local_file_name = key_f_res_se
-                message_id = final_response[key_f_res_se]
-                channel_id = str(message.chat.id)[4:]
-                private_link = f"https://t.me/c/{channel_id}/{message_id}"
-                message_to_send += "👉 <a href='"
-                message_to_send += private_link
-                message_to_send += "'>"
-                message_to_send += local_file_name
-                message_to_send += "</a>"
-                message_to_send += "\n"
-            if message_to_send != "":
-                mention_req_user = f"<a href='tg://user?id={usr_id}'>Your Requested Files</a>\n\n"
-                message_to_send = mention_req_user + message_to_send
-                message_to_send = message_to_send + "\n\n" + "#uploads"
-            else:
-                message_to_send = "<i>FAILED</i> to upload files. 😞😞"
-            await message.reply_text(
-                text=message_to_send,
-                quote=True,
-                disable_web_page_preview=True
+    try:
+        usr_id = message.from_user.id
+        if len(message.command) > 1:
+            new_name = '/app/' + message.command[1].strip()
+            file = await download_tg(client, message)
+            try:
+                if file:
+                    os.rename(file, new_name)
+            except Exception as g_g:
+                await message.reply_text("g_g")
+            response = {}
+            final_response = await upload_to_tg(
+                message,
+                new_name,
+                usr_id,
+                response
             )
-        except Exception as pe:
-            LOGGER.info(pe)
+            LOGGER.info(final_response)
+            try:
+                message_to_send = ""
+                for key_f_res_se in final_response:
+                    local_file_name = key_f_res_se
+                    message_id = final_response[key_f_res_se]
+                    channel_id = str(message.chat.id)[4:]
+                    private_link = f"https://t.me/c/{channel_id}/{message_id}"
+                    message_to_send += "👉 <a href='"
+                    message_to_send += private_link
+                    message_to_send += "'>"
+                    message_to_send += local_file_name
+                    message_to_send += "</a>"
+                    message_to_send += "\n"
+                if message_to_send != "":
+                    mention_req_user = f"<a href='tg://user?id={usr_id}'>Your Requested Files</a>\n\n"
+                    message_to_send = mention_req_user + message_to_send
+                    message_to_send = message_to_send + "\n\n" + "#uploads"
+                else:
+                    message_to_send = "<i>FAILED</i> to upload files. 😞😞"
+                await message.reply_text(
+                    text=message_to_send,
+                    quote=True,
+                    disable_web_page_preview=True
+                )
+            except Exception as pe:
+                LOGGER.info(pe)
 
-    else:
-        await message.reply_text("Provide new name of the file with extension 😐")
+        else:
+            await message.reply_text("Provide new name of the file with extension 😐")
+    except Exception as g_e:
+        LOGGER.info(g_e)
--- guru/plugins/rclone_size.py
+++ guru/plugins/rclone_size.py
@@ -19,32 +19,38 @@ from pyrogram.types import (
 
 async def check_size_g(client, message):
     #await asyncio.sleep(EDIT_SLEEP_TIME_OUT)
-    del_it = await message.reply_text("🔊 Checking size...wait!!!")
-    if not os.path.exists('rclone.conf'):
-        #subprocess.Popen(('touch', 'rclone.conf'), stdout = subprocess.PIPE)
-        with open('rclone.conf', 'a', newline="\n", encoding = 'utf-8') as fole:
-            fole.write("[DRIVE]\n")
-            fole.write(f"{RCLONE_CONFIG}")
-    destination = f'{DESTINATION_FOLDER}'
-    cmd = ['rclone', 'size', '--config=./rclone.conf', 'DRIVE:'f'{destination}']
-    gau_tam = await asyncio.create_subprocess_exec(*cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
-    gau, tam = await gau_tam.communicate()
-    print(gau)
-    print(tam)
-    print(tam.decode("utf-8"))
-    gautam = gau.decode("utf-8")
-    print(gautam)
-    await asyncio.sleep(5)
-    await message.reply_text(f"🔊CloudInfo:\n\n{gautam}")
-    await del_it.delete()
+    try:
+        del_it = await message.reply_text("🔊 Checking size...wait!!!")
+        if not os.path.exists('rclone.conf'):
+            #subprocess.Popen(('touch', 'rclone.conf'), stdout = subprocess.PIPE)
+            with open('rclone.conf', 'a', newline="\n", encoding = 'utf-8') as fole:
+                fole.write("[DRIVE]\n")
+                fole.write(f"{RCLONE_CONFIG}")
+        destination = f'{DESTINATION_FOLDER}'
+        cmd = ['rclone', 'size', '--config=./rclone.conf', 'DRIVE:'f'{destination}']
+        gau_tam = await asyncio.create_subprocess_exec(*cmd, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+        gau, tam = await gau_tam.communicate()
+        print(gau)
+        print(tam)
+        print(tam.decode("utf-8"))
+        gautam = gau.decode("utf-8")
+        print(gautam)
+        await asyncio.sleep(5)
+        await message.reply_text(f"🔊CloudInfo:\n\n{gautam}")
+        await del_it.delete()
+    except Exception as g_e:
+        LOGGER.info(g_e)
 
 #gautamajay52
 
 async def g_clearme(client, message):
-    inline_keyboard = []
-    ikeyboard = []
-    ikeyboard.append(InlineKeyboardButton("Yes 🚫", callback_data=("fuckingdo").encode("UTF-8")))
-    ikeyboard.append(InlineKeyboardButton("No 🤗", callback_data=("fuckoff").encode("UTF-8")))
-    inline_keyboard.append(ikeyboard)
-    reply_markup = InlineKeyboardMarkup(inline_keyboard)
-    await message.reply_text("Are you sure? 🚫 This will delete all your downloads locally 🚫", reply_markup=reply_markup, quote=True)
+    try:
+        inline_keyboard = []
+        ikeyboard = []
+        ikeyboard.append(InlineKeyboardButton("Yes 🚫", callback_data=("fuckingdo").encode("UTF-8")))
+        ikeyboard.append(InlineKeyboardButton("No 🤗", callback_data=("fuckoff").encode("UTF-8")))
+        inline_keyboard.append(ikeyboard)
+        reply_markup = InlineKeyboardMarkup(inline_keyboard)
+        await message.reply_text("Are you sure? 🚫 This will delete all your downloads locally 🚫", reply_markup=reply_markup, quote=True)
+    except Exception as g_e:
+        LOGGER.info(g_e)
